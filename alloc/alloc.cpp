059b6bd48ae859233bbaafb11855a0cd6dc205a4
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2018-07-16 23:55:53 +0800

    allocator

---
+++ alloc/alloc.cpp
@@ -0,0 +1,185 @@
+#include "alloc.h"
+#include <fstream>
+
+void file_alloc::delete_node(node *p)
+{
+    if (p->next)
+        p->next->prev = p->prev;
+    else
+        this->tail = p->prev;
+
+    if (p->prev)
+        p->prev->next = p->next;
+    else
+        this->head = p->next;
+
+    delete p;
+    return;
+}
+
+file_alloc::node *file_alloc::insert_before(node *p, off_t start, off_t end)
+{
+    node *newone = new node(start, end, p->prev, p);
+    if (p->prev)
+        p->prev->next = newone;
+    else
+        head = newone;
+
+    p->prev = newone;
+
+    return newone;
+}
+
+file_alloc::node *file_alloc::insert_tail(off_t start, off_t end)
+{
+    if (this->empty())
+    {
+        node *newone = new node(start, end);
+        head = tail = newone;
+        return newone;
+    }
+
+    node *newone = new node(start, end, tail);
+    tail->next = newone;
+    tail = newone;
+
+    return newone;
+}
+
+void file_alloc::merge(node *p)
+{
+    node *q;
+    if (p->next && p->next->start == p->end)
+    {
+        q = p->next;
+        p->end = q->end;
+        delete_node(q);
+    }
+    if (p->prev && p->prev->end == p->start)
+    {
+        q = p->prev;
+        q->end = p->end;
+        delete_node(p);
+    }
+}
+
+file_alloc::~file_alloc()
+{
+    this->clear();
+}
+
+void file_alloc::load(const string filename)
+{
+    clear();
+
+    fstream file;
+    file.open(filename, ios::binary);
+    if (!file)
+    {
+        cout << "File open wrong!" << endl;
+        return;
+    }
+
+    off_t nstart, nend;
+
+    file.read(reinterpret_cast<char *>(&fileend), sizeof(off_t));
+
+    while (!file.eof())
+    {
+        file.read(reinterpret_cast<char *>(&nstart), sizeof(off_t));
+        file.read(reinterpret_cast<char *>(&nend), sizeof(off_t));
+
+        insert_tail(nstart, nend);
+    }
+
+    file.close();
+}
+
+void file_alloc::dump(const string filename)
+{
+    fstream file;
+    file.open(filename, ios::binary);
+
+    file.write(reinterpret_cast<char *>(&fileend), sizeof(off_t));
+
+    node *p = this->head;
+    for (; p; p = p->next)
+    {
+        off_t nstart, nend;
+        nstart = p->start;
+        nend = p->end;
+        file.write(reinterpret_cast<char *>(&nstart), sizeof(off_t));
+        file.write(reinterpret_cast<char *>(&nend), sizeof(off_t));
+    }
+
+    file.close();
+}
+
+bool file_alloc::empty()
+{
+    return this->head == nullptr;
+}
+
+void file_alloc::clear()
+{
+    fileend = 0;
+
+    node *p, *q;
+    p = head;
+
+    while (p)
+    {
+        q = p->next;
+        delete p;
+        p = q;
+    }
+    head = tail = nullptr;
+}
+
+off_t file_alloc::alloc(size_t len)
+{
+    node *p = this->head;
+    off_t pos;
+
+    for (; p && (p->size()) < len; p = p->next)
+        ;
+
+    if (p)
+    {
+        pos = p->start;
+        p->start += len;
+        if (p->start == p->end)
+            delete_node(p);
+        return pos;
+    }
+    else
+    {
+        pos = fileend;
+        fileend += len;
+        return pos;
+    }
+}
+
+void file_alloc::free(off_t pos, size_t len)
+{
+    node *p = head, *q;
+    for (; p && p->start < pos; p = p->next)
+        ;
+
+    if (p)
+    {
+        if (p->start < pos + len)
+        {
+            cout << "Haven't alloc yet!" << endl;
+            return;
+        }
+        q = insert_before(p, pos, pos + len);
+    }
+    else
+    {
+        q = insert_tail(pos, pos + len);
+    }
+
+    merge(q);
+    return;
+}
---
+++ alloc/alloc.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+constexpr off_t invalid_off = 0xdeadbeef; //之前的无效
+
+class file_alloc
+{
+  private:
+    size_t nodesize = 4096;
+    struct node
+    {
+        off_t start, end;
+        node *prev, *next;
+        node(off_t start = 0, off_t end = 0, node *prev = nullptr, node *next = nullptr) : start(start), end(end), prev(prev), next(next){};
+        size_t size()
+        {
+            return this->end - this->start;
+        }
+        void print()
+        {
+            cout << "Block Start: " << this->start << ", End: " << this->end << endl;
+        }
+    };
+    node *head, *tail;
+    off_t fileend;
+
+    void delete_node(node *p);
+    node *insert_before(node *p, off_t start, off_t end);
+    node *insert_tail(off_t start, off_t end);
+    void merge(node *p);
+
+  public:
+    file_alloc() : head(nullptr), tail(nullptr), fileend(0){};
+    ~file_alloc();
+
+    void load(const string filename);
+    void dump(const string filename);
+
+    bool empty();
+    void clear();
+    off_t alloc(size_t len);
+    void free(off_t pos, size_t len);
+    void print()
+    {
+        node *p = this->head;
+        for (; p; p = p->next)
+        {
+            p->print();
+        }
+    }
+};
